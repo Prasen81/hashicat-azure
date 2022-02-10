cc122b42fc0dd86cbd31f27fc27fe293a60b7fdf
prch <prch@ramboll.dk>
2022-02-10 00:40:43 +0000

    new re,mote backend

--- remote_backend.tf
+++ remote_backend.tf
@@ -5,11 +5,11 @@ terraform {
     organization = "prchorg"
     workspaces {
       name = "hashicat-azure"
-=======
-    organization = "neworgprch"
-    workspaces {
-      name = "hashicat-azure-prch" # new comment 2
->>>>>>> b62521e9d22fd98ee9ae91fbc715e54713579c19
+#=======
+#    organization = "neworgprch"
+#    workspaces {
+#      name = "hashicat-azure-prch" # new comment 2
+#>>>>>>> b62521e9d22fd98ee9ae91fbc715e54713579c19
     }
   }
 }
